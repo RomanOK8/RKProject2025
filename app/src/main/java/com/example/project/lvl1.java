ca1bbda4de77dfe228c383388759ec8c04625914
Roman <roman_kory@mail.ru>
2024-04-30 13:40:24 +0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/project/lvl1.java
+++ app/src/main/java/com/example/project/lvl1.java
@@ -1,19 +1,17 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.annotation.SuppressLint;
+import android.animation.ValueAnimator;
 import android.content.Intent;
-import android.graphics.Typeface;
+import android.graphics.Rect;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -21,51 +19,47 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class lvl1 extends AppCompatActivity {
-    MediaPlayer mediaPlayera;
-    MediaPlayer mediaPlayerud;
+    private MediaPlayer mediaPlayera;
+    private MediaPlayer mediaPlayerud;
     private ImageView carImage;
     private float screenHeight;
-
-    //test
+    private TextView moveCounterTextView;
+    private Handler moveCounterHandler;
+    private TextView gameOverTextView;
     private Handler obstacleHandler;
     private Runnable createObstacleRunnable;
-    //test
-    private int moveCounter = 0; // Счётчик перемещений
-    private TextView moveCounterTextView;//counter
+    private boolean isGameOver = false;
 
-    @SuppressLint("WrongViewCast")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lvl1);
-        mediaPlayera=MediaPlayer.create(this,R.raw.pauseandbacksound);
-        mediaPlayerud=MediaPlayer.create(this,R.raw.upanddownbuttonsound);
         ImageView img = (ImageView)findViewById(R.id.swing_play);
         img.setBackgroundResource(R.drawable.background);
         AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
         frameAnimation.start();
-        //Изображение объекта
+        initMediaPlayers();
+        initViews();
+        startObstacleCreation();
+        startMoveCounter();
+    }
+
+    private void initMediaPlayers() {
+        mediaPlayera = MediaPlayer.create(this, R.raw.pauseandbacksound);
+        mediaPlayerud = MediaPlayer.create(this, R.raw.upanddownbuttonsound);
+    }
+
+    private void initViews() {
         carImage = findViewById(R.id.CarImage);
-        //Изображение объекта
-        //Для ограничения по y
-        DisplayMetrics displayMetrics = new DisplayMetrics();// Получаем высоту экрана
+        moveCounterTextView = findViewById(R.id.moveCounter);
+        gameOverTextView = findViewById(R.id.gameOverTextView);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         screenHeight = displayMetrics.heightPixels;
-        //Для ограничения по y
-        // Запускаем таймер для увеличения счётчика каждую секунду
-        moveCounterTextView = findViewById(R.id.moveCounter);
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                moveCounter++;
-                updateMoveCounter(); // Обновляем счётчик на экране
-                handler.postDelayed(this, 250);
-            }
-        }, 250);
-        //
-        //test
-        // Создаем препятствия каждые 3 секунды
+    }
+
+    private void startObstacleCreation() {
         obstacleHandler = new Handler();
         createObstacleRunnable = new Runnable() {
             @Override
@@ -75,68 +69,117 @@ public class lvl1 extends AppCompatActivity {
             }
         };
         obstacleHandler.postDelayed(createObstacleRunnable, 3000);
-        //test
     }
-    //test
-    private void createObstacle() {
-        // Создаем новый ImageView для препятствия
-        ImageView obstacle = new ImageView(this);
-        obstacle.setImageResource(R.drawable.obstacle); // Используем ресурс препятствия
 
-        // Устанавливаем размеры и позицию препятствия
+    private void startMoveCounter() {
+        moveCounterHandler = new Handler();
+        final Runnable moveCounterRunnable = new Runnable() {
+            private int moveCounter = 0;
+
+            @Override
+            public void run() {
+                moveCounter++;
+                updateMoveCounter(moveCounter);
+                moveCounterHandler.postDelayed(this, 250);
+            }
+        };
+        moveCounterHandler.postDelayed(moveCounterRunnable, 250);
+    }
+
+    private void createObstacle() {
+        ImageView obstacle = new ImageView(this);
+        obstacle.setImageResource(R.drawable.obstacle);
+        RelativeLayout.LayoutParams params = createObstacleLayoutParams();
+        obstacle.setLayoutParams(params);
+        RelativeLayout relativeLayout = findViewById(R.id.relativeLayout2);
+        relativeLayout.addView(obstacle);
+        animateObstacle(obstacle, relativeLayout);
+    }
+
+    private RelativeLayout.LayoutParams createObstacleLayoutParams() {
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT); // Начальное положение препятствия справа
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        params.topMargin = getRandomYPosition();
+        return params;
+    }
 
-        // Определяем верхнюю и нижнюю границы по y для генерации препятствия
-        int minY = 0; // Нижняя граница
-        int maxY = (int) (screenHeight - carImage.getHeight()); // Верхняя граница
-
-        // Генерируем случайное число в диапазоне от minY до maxY
+    private int getRandomYPosition() {
+        int minY = 0;
+        int maxY = (int) (screenHeight - carImage.getHeight());
         Random random = new Random();
-        int randomYPosition = random.nextInt(maxY - minY) + minY;
+        return random.nextInt(maxY - minY) + minY;
+    }
 
-        // Устанавливаем случайную позицию по y
-        params.topMargin = randomYPosition;
-        obstacle.setLayoutParams(params);
-
-        // Добавляем препятствие на экран
-        RelativeLayout relativeLayout = findViewById(R.id.relativeLayout2);
-        relativeLayout.addView(obstacle);
-
-        // Анимируем препятствие по горизонтали влево
+    private void animateObstacle(ImageView obstacle, RelativeLayout relativeLayout) {
         obstacle.animate()
                 .translationX(-relativeLayout.getWidth())
                 .setDuration(3000)
+                .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        checkCollisionWithObstacle(obstacle);
+                    }
+                })
                 .setListener(new AnimatorListenerAdapter() {
                     @Override
                     public void onAnimationEnd(Animator animation) {
-                        // Удаляем препятствие после анимации
                         relativeLayout.removeView(obstacle);
                     }
                 });
     }
-    //test
-    private void updateMoveCounter() {
+
+    private void checkCollisionWithObstacle(ImageView obstacle) {
+        if (isColliding(carImage, obstacle)) {
+            gameOver();
+        }
+    }
+
+    private boolean isColliding(ImageView imageView1, ImageView imageView2) {
+        Rect rect1 = new Rect();
+        imageView1.getGlobalVisibleRect(rect1);
+        Rect rect2 = new Rect();
+        imageView2.getGlobalVisibleRect(rect2);
+        return Rect.intersects(rect1, rect2);
+    }
+
+    private void gameOver() {
+        isGameOver = true;
+        obstacleHandler.removeCallbacks(createObstacleRunnable);
+        if (moveCounterHandler != null) {
+            moveCounterHandler.removeCallbacksAndMessages(null); // Остановить счетчик перемещений
+        }
+        carImage.clearAnimation();
+        gameOverTextView.setVisibility(View.VISIBLE);
+        gameOverTextView.setText("Game Over");
+    }
+    private void updateMoveCounter(int moveCounter) {
         moveCounterTextView.setText(String.valueOf(moveCounter));
     }
-    public void pauseButton(View v){
-        Intent intent=new Intent(this,Pausemenu.class);
+
+    public void pauseButton(View v) {
+        Intent intent = new Intent(this, Pausemenu.class);
         startActivity(intent);
         mediaPlayera.start();
     }
-    public void upButton(View v){//метод для управления по y вверх+ограничение движения вверх
-        if (carImage.getY() > 0) {
-            carImage.animate().translationYBy(-100).setDuration(100);
-            mediaPlayerud.start();
-        }
 
+    public void upButton(View v) {
+        moveCar(-100);
     }
-    public void downButton(View v){//метод для управления по y вниз +ограничение движения вниз
-        if (carImage.getY() < screenHeight - carImage.getHeight()) {
-            carImage.animate().translationYBy(100).setDuration(100);
+
+    public void downButton(View v) {
+        moveCar(100);
+    }
+
+    private void moveCar(int distance) {
+        if (!isGameOver && isWithinBounds(carImage.getY() + distance)) {
+            carImage.animate().translationYBy(distance).setDuration(100);
             mediaPlayerud.start();
         }
+    }
+
+    private boolean isWithinBounds(float newY) {
+        return newY > 0 && newY < screenHeight - carImage.getHeight();
     }
 }
