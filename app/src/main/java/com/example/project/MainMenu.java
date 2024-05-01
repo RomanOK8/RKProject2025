d8b56f8d570ed84da477e642f42790d09daf3ef1
Roman <roman_kory@mail.ru>
2024-05-01 18:49:55 +0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/project/MainMenu.java
+++ app/src/main/java/com/example/project/MainMenu.java
@@ -12,6 +12,7 @@ public class MainMenu extends AppCompatActivity {
     MediaPlayer mediaPlayerm;
     MediaPlayer mediaPlayere;
     MediaPlayer mediaPlayers;
+    MediaPlayer mediaPlayerp;
     @Override
     @SuppressLint("WrongViewCast")
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +21,42 @@ public class MainMenu extends AppCompatActivity {
         mediaPlayerm=MediaPlayer.create(this,R.raw.playbuttonsound);
         mediaPlayere=MediaPlayer.create(this,R.raw.endsound);
         mediaPlayers=MediaPlayer.create(this,R.raw.settingsbuttonsound);
-        // LoopFW loopFW=new LoopFW();
-        //loopFW.startGame();
+        mediaPlayerp=MediaPlayer.create(this,R.raw.mainmusic);
+        mediaPlayerp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // Воспроизведение заново, когда трек заканчивается
+                mp.start();
+            }
+        });
+        mediaPlayerp.start();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Освобождение ресурсов MediaPlayer
+        if (mediaPlayerp != null) {
+            mediaPlayerp.release();
+            mediaPlayerp = null;
+        }
     }
     public void startNewActivity(View v){
         Intent intent=new Intent(this, LevelMenu.class);
         startActivity(intent);
         mediaPlayerm.start();
+        mediaPlayerp.stop();
     }
     public void startNewActivity2(View v){
         Intent intent=new Intent(this, Options.class);
         startActivity(intent);
         mediaPlayers.start();
+        mediaPlayerp.stop();
     }
     public void end(View v){
         Intent intent=new Intent(this,areusure.class);
         startActivity(intent);
         mediaPlayere.start();
+        mediaPlayerp.stop();
+
     }
 }
