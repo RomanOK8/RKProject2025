ec58aa68c20b696f29f4e93022e4f9370658b134
Roman <roman_kory@mail.ru>
2024-05-01 12:05:27 +0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/project/lvl2.java
+++ app/src/main/java/com/example/project/lvl2.java
@@ -2,13 +2,34 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class lvl2 extends AppCompatActivity {
+    private RelativeLayout relativeLayout;
+    MediaPlayer mediaPlayera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lvl2);
+        ImageView Img = (ImageView)findViewById(R.id.backgroundlvl2);
+        Img.setBackgroundResource(R.drawable.lvl2_background);
+        AnimationDrawable frameAnimation = (AnimationDrawable) Img.getBackground();
+        frameAnimation.start();
+        initMediaPlayers();
+    }
+    public void pauseButton(View v) {
+        Intent intent = new Intent(this, LevelMenu.class);
+        startActivity(intent);
+        mediaPlayera.start();
+    }
+    private void initMediaPlayers() {
+        mediaPlayera = MediaPlayer.create(this, R.raw.pauseandbacksound);
     }
 }
