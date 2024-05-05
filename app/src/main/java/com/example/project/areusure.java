1e8ad6da6a2b5ebcd3d74f7e2d46edb1e6b4fac4
Roman <roman_kory@mail.ru>
2024-05-05 10:16:09 +0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/project/areusure.java
+++ app/src/main/java/com/example/project/areusure.java
@@ -6,31 +6,67 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class areusure extends AppCompatActivity {
     MediaPlayer mediaPlayere;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_areusure);
-        mediaPlayere=MediaPlayer.create(this,R.raw.endsound);
+        mediaPlayere = MediaPlayer.create(this, R.raw.endsound);
         ImageView Img = (ImageView)findViewById(R.id.lol_play);
         Img.setBackgroundResource(R.drawable.backgroundareusreactivity);
 
         AnimationDrawable frameAnimation = (AnimationDrawable) Img.getBackground();
 
         frameAnimation.start();
-    }
-    public void end(View v){
-        this.finishAffinity();
-        mediaPlayere.start();
-    }
-    public void startNewActivity(View v){
-        this.finishAffinity();
-        Intent intent=new Intent(this, MainMenu.class);
-        startActivity(intent);
-        mediaPlayere.start();
+        // Установите слушатели нажатий для кнопок
+        ImageButton endButton = findViewById(R.id.leavebt);
+        ImageButton startNewActivityButton = findViewById(R.id.bkbt3);
+
+        // Метод для установки анимации прозрачности
+        final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.5F);
+        buttonClick.setDuration(0); // Длительность анимации в миллисекундах
+        buttonClick.setFillAfter(true); // Сохраняет конечное состояние после анимации
+
+        final AlphaAnimation buttonRelease = new AlphaAnimation(0.5F, 1F);
+        buttonRelease.setDuration(0); // Длительность анимации в миллисекундах
+        buttonRelease.setFillAfter(true); // Сохраняет конечное состояние после анимации
+
+        View.OnTouchListener changeAlphaOnTouch = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.startAnimation(buttonClick);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        v.startAnimation(buttonRelease);
+                        if (v.getId() == R.id.leavebt) {
+                            finishAffinity();
+                            mediaPlayere.start();
+                        } else if (v.getId() == R.id.bkbt3) {
+                            Intent intent = new Intent(areusure.this, MainMenu.class);
+                            startActivity(intent);
+                            mediaPlayere.start();
+                        }
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        v.startAnimation(buttonRelease);
+                        break;
+                }
+                return true;
+            }
+        };
+
+        endButton.setOnTouchListener(changeAlphaOnTouch);
+        startNewActivityButton.setOnTouchListener(changeAlphaOnTouch);
     }
 }
